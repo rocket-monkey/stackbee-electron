cbdb87fc05e06f04d57d8c66566df336f99af3fe
Remo Vetere <remo.vetere@gmail.com>
2018-08-27 01:28:39 +0200

    try to make it work on linux too

--- scripts/fixHttpExecutor.sh
+++ scripts/fixHttpExecutor.sh
@@ -3,4 +3,8 @@
 # stupid workaround to make electron-builder publish work
 # -> next installs it's own package "url" so we end up not using the native module anymore in "httpExecutor.js" of "builder-util-runtime", so we need to change a className from "URL" to "Url" -.-
 
-sed -i '' 's/      const parsedNewUrl = new (_url().URL)(redirectUrl);/      const parsedNewUrl = new (_url().Url)(redirectUrl);/' node_modules/builder-util-runtime/out/httpExecutor.js
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  sed -i 's/      const parsedNewUrl = new (_url().URL)(redirectUrl);/      const parsedNewUrl = new (_url().Url)(redirectUrl);/' node_modules/builder-util-runtime/out/httpExecutor.js
+else
+  sed -i '' 's/      const parsedNewUrl = new (_url().URL)(redirectUrl);/      const parsedNewUrl = new (_url().Url)(redirectUrl);/' node_modules/builder-util-runtime/out/httpExecutor.js
+fi
