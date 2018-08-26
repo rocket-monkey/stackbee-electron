0037c7c5bd70d2afef4dff912e1e372555191464
Remo Vetere <remo.vetere@gmail.com>
2018-08-27 01:11:46 +0200

    nice fix with sed

--- scripts/fixHttpExecutor.sh
+++ scripts/fixHttpExecutor.sh
@@ -3,4 +3,4 @@
 # stupid workaround to make electron-builder publish work
 # -> next installs it's own package "url" so we end up not using the native module anymore in "httpExecutor.js" of "builder-util-runtime", so we need to change a className from "URL" to "Url" -.-
 
-while read a ; do echo ${a//_url().URL/_url().Url} ; done < ./node_modules/builder-util-runtime/out/httpExecutor.js > ./node_modules/builder-util-runtime/out/httpExecutor.js.t ; mv ./node_modules/builder-util-runtime/out/httpExecutor.js{.t,}
+sed -i '' 's/      const parsedNewUrl = new (_url().URL)(redirectUrl);/      const parsedNewUrl = new (_url().Url)(redirectUrl);/' node_modules/builder-util-runtime/out/httpExecutor.js
