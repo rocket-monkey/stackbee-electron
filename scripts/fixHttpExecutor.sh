de4823a8b69c93b507cb72ff55de607d3bbc7d08
Remo Vetere <remo.vetere@gmail.com>
2018-08-27 02:07:54 +0200

    woot

--- scripts/fixHttpExecutor.sh
+++ scripts/fixHttpExecutor.sh
@@ -4,7 +4,7 @@
 # -> next installs it's own package "url" so we end up not using the native module anymore in "httpExecutor.js" of "builder-util-runtime", so we need to change a className from "URL" to "Url" -.-
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-  sed -i 's/      const parsedNewUrl = new (_url().URL)(redirectUrl);/      const parsedNewUrl = new (_url().Url)(redirectUrl);/' node_modules/builder-util-runtime/out/httpExecutor.js
+  # sed -i 's/      const parsedNewUrl = new (_url().URL)(redirectUrl);/      const parsedNewUrl = new (_url().Url)(redirectUrl);/' node_modules/builder-util-runtime/out/httpExecutor.js
 else
   sed -i '' 's/      const parsedNewUrl = new (_url().URL)(redirectUrl);/      const parsedNewUrl = new (_url().Url)(redirectUrl);/' node_modules/builder-util-runtime/out/httpExecutor.js
 fi
